507af437c070b4ae62290643270ed35ffdf22a82
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-05-28 01:59:55 +0200

    Add EnrollRequest DTO with required CourseId property
    Introduced EnrollRequest class in the DTOs namespace to represent enrollment requests. The class includes a required CourseId property to ensure valid enrollment data.

---
+++ Lms.EnrollmentService.Application/DTOs/EnrollRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lms.EnrollmentService.Application.DTOs;
+
+public class EnrollRequest
+{
+    [Required]
+    public Guid CourseId { get; set; }
+}
