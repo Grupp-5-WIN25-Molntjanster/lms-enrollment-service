5dc137bc28705a8f62f129dff35499bcb6e53a04
Riad Alrashed <m.riad.alrashed@gmail.com>
2026-05-29 17:51:39 +0200

    Add unit test for WaitlistEntry creation logic
    Introduced WaitlistEntryTests using xUnit and FluentAssertions.
    Test verifies correct property assignment and timestamp on creation.

---
+++ Lms.EnrollmentService.UnitTests/Domain/Entities/WaitlistEntryTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Lms.EnrollmentService.Domain.Entities;
+
+namespace Lms.EnrollmentService.UnitTests.Domain;
+
+public class WaitlistEntryTests
+{
+    [Fact]
+    public void Create_WithValidData_ShouldSucceed()
+    {
+        var userId = Guid.NewGuid();
+        var courseId = Guid.NewGuid();
+        var position = Guid.NewGuid();
+
+        var entry = new WaitlistEntry(userId, courseId, position);
+
+        entry.UserId.Should().Be(userId);
+        entry.CourseId.Should().Be(courseId);
+        entry.Position.Should().Be(position);
+        entry.AddedAt.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromSeconds(5));
+    }
+}
